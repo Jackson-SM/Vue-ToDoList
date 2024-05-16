60b2ff04bad00e807cd17a9dc31bd4cdadbf0221
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2024-05-15 23:39:25 -0300

    feat: creating circle button to open a dropdown or add new things

---
+++ src/components/ButtonCircle.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "ButtonCircle",
+});
+</script>
+
+<template>
+    <button class="button-circle">
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+.button-circle {
+    background-color: var(--background);
+    color: var(--text-foreground);
+    border: none;
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    transition: background-color 0.3s;
+}
+
+.button-circle:hover {
+    background-color: var(--background-hover);
+}
+</style>
