689aa58454cffe1cf52291b9965739d4f4307291
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2024-05-15 23:39:03 -0300

    feat: create Badge component for the tags on task card

---
+++ src/components/Badge.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "BadgeComponent",
+});
+</script>
+
+<template>
+    <span class="badge">
+        <slot></slot>
+    </span>
+</template>
+
+<style scoped>
+.badge {
+    font-size: 0.7rem;
+    border-radius: 10px;
+    background-color: var(--muted-purple);
+    padding: 0.2rem 0.6rem;
+}
+</style>
